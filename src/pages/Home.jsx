f301674cb907144757f7e61d9469c5eef107c975
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-04-22 10:08:53 +0200

    Steup store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import {state} from '../store';
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+
+  const snap = useSnapshot(state);
+  return <div>Home</div>;
+};
 
 export default Home;
