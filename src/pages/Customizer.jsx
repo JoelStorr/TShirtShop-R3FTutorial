5229036a973cedf20cd75e9ccce0be25dbb0611b
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-04-22 20:52:00 +0200

    Added image upload tab

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -39,7 +39,11 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />
       case "filepicker": 
-        return <FilePicker/>
+        return <FilePicker
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
+        />
       case "aipicker":
         return <AIPicker />
       default:
@@ -47,7 +51,45 @@ const Customizer = () => {
     }
   }
 
+  const handleDecals = (type, result) =>{
+    const decalType = DecalTypes[type];
+    state[decalType.stateProperty] = result;
 
+    if(!activeFilterTab[decalType.filterTab]){
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  }
+
+
+  const handleActiveFilterTab = (tabName) => {
+    switch(tabName){
+
+      case "logoShirt":
+          state.isLogoTexture = !activeFilterTab[tabName];
+          break;
+      case 'stylishShirt':
+          state.isFullTexture = !activeFilterTab[tabName];
+      default:
+          state.isFullTexture = true;
+          state.isLogoTexture = false;
+    }
+
+    //setactive filter tab to update the UI
+    setActiveFilterTab((prevState)=>{
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
+  }
+
+
+  const readFile = (type)=>{
+    reader(file).then((result)=>{
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    })
+  }
 
   return (
     <AnimatePresence>
@@ -95,9 +137,9 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
+                isActiveTab={activeFilterTab[tab.name]}
                 handleClick={() => {
-                  
+                  handleActiveFilterTab(tab.name)
                 }}
               />
             ))}
